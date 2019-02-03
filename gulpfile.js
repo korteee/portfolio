d780110fcdd3f8d6337431deb3f9477b4c5b70bd
Korte-PC\Korte <korte.stavros@hotmail.com>
2019-02-03 13:22:17 +0200

    fix(menu): fix icon sizes

--- gulpfile.js
+++ gulpfile.js
@@ -13,139 +13,139 @@ const runSequence = require('run-sequence');
 const replace = require('gulp-replace');
 const merge = require('merge-stream');
 
-
-
-const files = { in: {
-        styles: './src/css/styles.css',
-        js: [
-            './node_modules/jquery/dist/jquery.min.js',
-            './node_modules/jquery-mousewheel/jquery.mousewheel.js',
-            './node_modules/jquery-touchswipe/jquery.touchSwipe.min.js',
-            './node_modules/iscroll/build/iscroll.js',
-            './node_modules/babel-polyfill/dist/polyfill.min.js',
-            './node_modules/jquery-validation/dist/jquery.validate.min.js',
-            './node_modules/toastr/build/toastr.min.js',
-            './src/js/*'
-        ],
-        assets: ['./src/fonts/**', './src/img/**', './src/videos/**'],
-        favicon: './favicon/**'
-    },
-    out: {
-        styles: `./dist/styles.css`,
-        js: `./dist/scripts.js`,
-        assets: `./dist`
-    }
-}
-
-
+const files = {
+  in: {
+    styles: './src/css/styles.css',
+    js: [
+      './node_modules/jquery/dist/jquery.min.js',
+      './node_modules/jquery-mousewheel/jquery.mousewheel.js',
+      './node_modules/jquery-touchswipe/jquery.touchSwipe.min.js',
+      './node_modules/iscroll/build/iscroll.js',
+      './node_modules/babel-polyfill/dist/polyfill.min.js',
+      './node_modules/jquery-validation/dist/jquery.validate.min.js',
+      './node_modules/toastr/build/toastr.min.js',
+      './src/js/*'
+    ],
+    assets: ['./src/fonts/**', './src/img/**', './src/videos/**'],
+    favicon: './favicon/**'
+  },
+  out: {
+    styles: `./dist/styles.css`,
+    js: `./dist/scripts.js`,
+    assets: `./dist`
+  }
+};
 
 gulp.task('clean-css', () => {
-    return gulp.src('src/css/styles.css')
-        .pipe(clean())
-})
+  return gulp.src('src/css/styles.css').pipe(clean());
+});
 
-gulp.task('sass', function () {
-    return gulp.src('src/sass/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(cleanCSS({
-            compatibility: 'ie10',
-            inline: ['local']
-        }))
-        .pipe(rename('styles.css'))
-        .pipe(gulp.dest('./src/css'))
-        .pipe(browserSync.stream({
-            match: '**/*.css'
-        }));
+gulp.task('sass', function() {
+  return gulp
+    .src('src/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(
+      autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+      })
+    )
+    .pipe(
+      cleanCSS({
+        compatibility: 'ie10',
+        inline: ['local']
+      })
+    )
+    .pipe(rename('styles.css'))
+    .pipe(gulp.dest('./src/css'))
+    .pipe(
+      browserSync.stream({
+        match: '**/*.css'
+      })
+    );
 });
 
 gulp.task('browserSync', ['sass'], () => {
-    var filesToWatch = [
-        'src/sass/**/*.scss',
-        "index.html",
-        "src/js/**/*.js"
-    ]
+  var filesToWatch = ['src/sass/**/*.scss', 'index.html', 'src/js/**/*.js'];
 
-    browserSync.init(filesToWatch, {
-        server: true,
-        injectChanges: true
-    });
-})
-
-gulp.task('serve', ['sass', 'browserSync'], function () {
-    gulp.watch('src/sass/**/*.scss', ['sass']);
-    gulp.watch("index.html").on('change', browserSync.reload);
-    gulp.watch("src/js/**/*.js", ['idd']);
+  browserSync.init(filesToWatch, {
+    server: true,
+    injectChanges: true
+  });
 });
 
-gulp.task('scripts', function () {
+gulp.task('serve', ['sass', 'browserSync'], function() {
+  gulp.watch('src/sass/**/*.scss', ['sass', 'idd']);
+  gulp.watch('index.html').on('change', browserSync.reload);
+  gulp.watch('src/js/**/*.js', ['idd']);
+});
 
-    gulp.src(files.in.js)
-        .pipe(babel({
-            presets: ['es2015']
-        }))
-        .pipe(uglify({
-            compress: {
-                drop_console: true
-            }
-        }))
-        .pipe(concat('scripts.js'))
-        .pipe(gulp.dest('./dist'));
-})
+gulp.task('scripts', function() {
+  gulp
+    .src(files.in.js)
+    .pipe(
+      babel({
+        presets: ['es2015']
+      })
+    )
+    .pipe(
+      uglify({
+        compress: {
+          drop_console: true
+        }
+      })
+    )
+    .pipe(concat('scripts.js'))
+    .pipe(gulp.dest('./dist'));
+});
 
-gulp.task('styles', ['sass'], function () {
+gulp.task('styles', ['sass'], function() {
+  gulp
+    .src(files.in.styles)
+    .pipe(replace('../fonts', './fonts'))
+    .pipe(replace('../img', './img'))
+    .pipe(gulp.dest('./dist'));
+});
 
-    gulp.src(files.in.styles)
-        .pipe(replace('../fonts', './fonts'))
-        .pipe(replace('../img', './img'))
-        .pipe(gulp.dest('./dist'))
+gulp.task('copy-assets', function() {
+  const assets = gulp.src(files.in.assets, {
+    base: './src'
+  });
+  const favicon = gulp.src(files.in.favicon, {
+    base: '.'
+  });
 
-})
-
-gulp.task('copy-assets', function () {
-    const assets = gulp.src(files.in.assets, {
-        base: './src'
-    });
-    const favicon = gulp.src(files.in.favicon, {
-        base: '.'
-    });
-
-    merge(assets, favicon)
-        .pipe(gulp.dest(files.out.assets));
-})
+  merge(assets, favicon).pipe(gulp.dest(files.out.assets));
+});
 
 //Inject development dependencies to index
 gulp.task('idd', ['sass'], () => {
-    const target = gulp.src("./index.html");
-    const sources = gulp.src([files.in.styles].concat(files.in.js));
-    return target.pipe(inject(sources))
-        .pipe(gulp.dest('./'))
-})
-
+  const target = gulp.src('./index.html');
+  const sources = gulp.src([files.in.styles].concat(files.in.js));
+  return target.pipe(inject(sources)).pipe(gulp.dest('./'));
+});
 
 //Inject production dependencies
-gulp.task('ipd', function () {
-    setTimeout(function () {
-        const target = gulp.src("./index.html");
+gulp.task('ipd', function() {
+  setTimeout(function() {
+    const target = gulp.src('./index.html');
 
-        console.log([files.out.js].concat(files.out.styles))
-        const sources = gulp.src([files.out.js].concat(files.out.styles));
-        return target.pipe(inject(sources, {
-                relative: true
-            }))
-            .pipe(gulp.dest('./'))
-    }, 8000);
-})
+    console.log([files.out.js].concat(files.out.styles));
+    const sources = gulp.src([files.out.js].concat(files.out.styles));
+    return target
+      .pipe(
+        inject(sources, {
+          relative: true
+        })
+      )
+      .pipe(gulp.dest('./'));
+  }, 8000);
+});
 
-gulp.task('clean-dist', function () {
-    gulp.src('./dist')
-        .pipe(clean());
-})
+gulp.task('clean-dist', function() {
+  gulp.src('./dist').pipe(clean());
+});
 
-
-gulp.task('build', function () {
-    runSequence('clean-dist', 'styles', 'scripts', 'copy-assets', 'ipd')
-})
+gulp.task('build', function() {
+  runSequence('clean-dist', 'styles', 'scripts', 'copy-assets', 'ipd');
+});
